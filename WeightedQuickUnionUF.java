b5493de62750426f2f493774c5d5b911d7e5fd69
LDebris <2472903575@qq.com>
2023-01-11 11:34:28 +0800

    add UF

---
+++ WeightedQuickUnionUF.java
@@ -0,0 +1,65 @@
+public class WeightedQuickUnionUF {
+    private int[] parent;
+    private int[] size;
+    private int count;
+
+    public WeightedQuickUnionUF(int n) {
+        count = n;
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i<n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public int find(int p) {
+        validate(p);
+        while(p != parent[p])
+            p = parent[p];
+        return p;
+    }
+
+    private void validate(int p) {
+        int n = parent.length;
+        if(p<0 || p>=n) {
+            throw new IllegalArgumentException("index" + p + "is not between 0 and " + (n-1));
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if(rootQ == rootP) return;
+        if(size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        else {
+            parent[rootQ] = rootP;
+            size[rootP] += rootQ;
+        }
+        count--;
+    }
+
+    public static void  main(String[] args) {
+        int n = StdIn.readInt();
+         WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+        while(!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if(uf.find(p) == uf.find(q)) continue;
+            uf.union(p,q);
+            Stdout.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + "components")
+
+}
